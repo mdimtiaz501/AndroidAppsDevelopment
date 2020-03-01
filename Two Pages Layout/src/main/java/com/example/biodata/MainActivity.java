30114139282558f7292566b11d44b2f1125b2b70
Md Imtiaz Chowdhury <39957822+mdimtiaz501@users.noreply.github.com>
2020-03-01 17:28:50 +0600

    Add files via upload

---
+++ Two Pages Layout/src/main/java/com/example/biodata/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.biodata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                secondActivity();
+            }
+        });
+    }
+    public void secondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
